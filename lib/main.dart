bf7046d890a13ed480c2384e14800412b2f634e5
Muhammad Haris <369haris@gmail.com>
2022-03-11 17:05:57 +0500

    Profile Screen route added

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ui/constants/constants.dart';
+import 'package:ui/ui/screens/dashboard.dart';
+import 'package:ui/ui/screens/profile/profile_screen.dart';
+import 'package:ui/ui/screens/signin.dart';
+import 'package:ui/ui/screens/signup.dart';
+import 'package:provider/provider.dart';
+import 'package:ui/ui/widgets/header.dart';
+
+import 'ui/screens/home.dart';
+import 'utils/menu_controller.dart';
+// import 'package:ui/ui/splashscreen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MultiProvider(
+      providers: [
+            ChangeNotifierProvider<MenuController>(
+              create: (context) => MenuController(),
+            ),
+          ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Login",
+        // theme: ThemeData(primaryColor: Colors.orange[200]),
+        routes: <String, WidgetBuilder>{
+          SIGN_IN: (BuildContext context) =>  const SignInPage(),
+          SIGN_UP: (BuildContext context) =>  const SignUpScreen(),
+          DASHBOARD: (BuildContext context) =>  const DashBoardScreen(),
+          HOME: (BuildContext context) => const Home(),
+          PROFILE: (BuildContext context) => ProfileScreen(),
+        },
+        initialRoute: SIGN_IN,
+        // home: PopoverExample(),
+       
+      ),
+    );
+  }
+}
+
+
+
