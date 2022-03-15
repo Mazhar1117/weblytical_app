bb173cfcf1f112b4a35f62b148d1326401577031
Muhammad Haris <369haris@gmail.com>
2022-03-15 23:27:16 +0500

    Login/SignUp screen UI updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ui/constants/constants.dart';
+import 'package:ui/ui/screens/authentication/login_screen.dart';
 import 'package:ui/ui/screens/dashboard.dart';
+import 'package:ui/ui/screens/diagnostics/diagnostics_screen.dart';
 import 'package:ui/ui/screens/profile/profile_screen.dart';
-import 'package:ui/ui/screens/signin.dart';
-import 'package:ui/ui/screens/signup.dart';
+import 'package:ui/ui/screens/register/sign_up_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:ui/ui/widgets/header.dart';
-
 import 'ui/screens/home.dart';
 import 'utils/menu_controller.dart';
 // import 'package:ui/ui/splashscreen.dart';
@@ -26,28 +25,25 @@ class MyApp extends StatelessWidget {
     ]);
     return MultiProvider(
       providers: [
-            ChangeNotifierProvider<MenuController>(
-              create: (context) => MenuController(),
-            ),
-          ],
+        ChangeNotifierProvider<MenuController>(
+          create: (context) => MenuController(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Login",
         // theme: ThemeData(primaryColor: Colors.orange[200]),
         routes: <String, WidgetBuilder>{
-          SIGN_IN: (BuildContext context) =>  const SignInPage(),
-          SIGN_UP: (BuildContext context) =>  const SignUpScreen(),
-          DASHBOARD: (BuildContext context) =>  const DashBoardScreen(),
+          SIGN_IN: (BuildContext context) => LoginScreen(),
+          SIGN_UP: (BuildContext context) => SignupScreen(),
+          DASHBOARD: (BuildContext context) => const DashBoardScreen(),
           HOME: (BuildContext context) => const Home(),
           PROFILE: (BuildContext context) => ProfileScreen(),
+          DIAGNOSTICS: (BuildContext context) => DiagnosticsScreen()
         },
         initialRoute: SIGN_IN,
         // home: PopoverExample(),
-       
       ),
     );
   }
 }
-
-
-
